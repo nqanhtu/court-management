1f8bce2ef151c8e992371b2bd7288903106e1df4
Tu Nguyen <nqanhtu@outlook.com>
2026-06-08 20:46:36 +0700

    feat(web): open create file dialog via URL param

--- src/routes/files/files-page.tsx
+++ src/routes/files/files-page.tsx
@@ -1,15 +1,28 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { CreateFileDialog } from '@/components/create-file-dialog'
 import { OverviewStats } from '@/components/overview-stats'
 import { FileListSection } from '@/components/files/file-list-section'
 import { DataPageShell } from '@/components/common/data-page-shell'
 import { PageHeader } from '@/components/common/page-header'
+import { useSearchParams, useRouter } from '@/src/lib/router'
 
 export default function Home() {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const createParam = searchParams.get('create')
+
+  useEffect(() => {
+    if (createParam === 'true') {
+      setIsCreateModalOpen(true)
+      const params = new URLSearchParams(searchParams.toString())
+      params.delete('create')
+      router.replace(`/?${params.toString()}`)
+    }
+  }, [createParam, searchParams, router])
 
   return (
     <DataPageShell
